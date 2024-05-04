d2a2adce03809f211104af4e6a8e85da28f7826f
MIIIIDAS <berentadrian@gmail.com>
2024-05-04 14:46:58 +0200

    Dodaj pliki projektów.

---
+++ proNETzadanie3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string sciezkaDoPliku = "plik.txt"; // wpisz ścieżkę do pliku
+
+        if (File.Exists(sciezkaDoPliku))
+        {
+            using (FileStream strumienPliku = new FileStream(sciezkaDoPliku, FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader czytnik = new StreamReader(strumienPliku))
+                {
+                    string zawartoscPliku = czytnik.ReadToEnd();
+                    Console.WriteLine("Zawartość pliku:");
+                    Console.WriteLine(zawartoscPliku);
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Plik nie istnieje.");
+        }
+
+        Console.ReadLine();
+    }
+}
